8470276e9683ce20c612863db729f74ed34ad07f
andrew <andyhaz1970@gmail.com>
2024-03-03 20:55:07 -0600

    Initial Commit

---
+++ SwiftUI-Draw/SwiftUI_DrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DrawApp.swift
+//  SwiftUI-Draw
+//
+//  Created by andyhaz on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
