b0828620d18028b57cdc56aef3e2808991de4223
andrew <andyhaz1970@gmail.com>
2024-03-05 13:22:06 -0600

    working version

--- SwiftUI-Draw/ContentView.swift
+++ SwiftUI-Draw/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x:rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Triangle()
+                .stroke(.blue, style: StrokeStyle(lineWidth: 5,lineCap: .round,lineJoin: .round))
+                .fill(.green)
+                .frame(width: 300, height: 300)
+                .background(.gray)
+              //  .position(x: 0.0,y: 0.0)
+              //  .rotationEffect(.degrees(25.0))
         }
-        .padding()
+        
     }
 }
 
